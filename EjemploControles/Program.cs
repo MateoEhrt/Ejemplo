eaa98b3fe3d0bd0d29674a028c70f5ae5269ad5a
unknown <mateoehrt@gmail.com>
2022-10-04 11:20:18 -0300

    Modificación ejemplo

--- EjemploControles/Program.cs
+++ EjemploControles/Program.cs
@@ -17,6 +17,9 @@ namespace EjemploControles
             Application.EnableVisualStyles();
             Application.SetCompatibleTextRenderingDefault(false);
             Application.Run(new frmPrincipal());
+
         }
+
+        //Comentario ejemplo github
     }
 }
